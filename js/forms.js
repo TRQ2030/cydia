e452b8b1c4d3d3568826cdf5529166015baaa61c
TRQ2030 <55149636+TRQ2030@users.noreply.github.com>
2024-11-24 07:52:48 +0300

    Update forms.js

--- js/forms.js
+++ js/forms.js
@@ -1,1 +1,10 @@
-d84c325f36814f39bbe59080272b10c3:550727de4c96ef1ecff874905493580f
+player.configure({
+  manifest: {
+    dash: {
+      keySystemsByURI: {
+        'urn:uuid:d84c325f36814f39bbe59080272b10c3': '550727de4c96ef1ecff874905493580f',
+        'urn:uuid:d84c325f36814f39bbe59080272b10c3': '550727de4c96ef1ecff874905493580f',
+      }
+  }
+  }
+});
